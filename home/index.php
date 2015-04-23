7f74b3d2f83e3912d5cffbfdfc525baa67d8429a
Hehfay <jeffrey.hartman4691@gmail.com>
2015-04-23 04:27:18 -0400

    Added idea for alumni map.

--- home/index.php
+++ home/index.php
@@ -18,9 +18,40 @@ echo "<title>PHA | ".ucwords(end($name))."</title>";
 </ul>
 <?php
 require_once "global.php";
-echo "<p>Welcome to the President's House Association website.</p>";
+echo "<p>Welcome to the President's House Association website.<br>";
 $this_many = date("Y") - FOUNDING_YEAR;
-echo "Celebrating $this_many years of excellence.";
+echo "Celebrating $this_many years of excellence.<br> Checkout where some of our Alumini are living:</p>";
+
+$img_src = "\"https://maps.googleapis.com/maps/api/staticmap?center=United+States&size=800x800&maptype=roadmap
+&markers=color:blue%size:small%|";
+
+$mysqli = new mysqli($host, $username, $password, $database);
+if($mysqli->connect_errno)
+{
+  echo "Connect Error: $mysqli->connect_errno";
+  exit(1);
+}
+else
+{
+  $query = "select Address_Line_1, City, State from Alumni where Address_Line_1 != 'TODO'";
+  if($result = $mysqli->query($query))
+  {
+    for($i = 0; $i < 40; $i++) // TODO Allow user to view alum locations by class.
+    {
+      $row = $result->fetch_row();
+      $img_src .= $row[0]." ".$row[1]." ".$row[2]."|";
+    }
+    rtrim($img_src, "|");
+    $img_src .= "\">";
+
+    echo "<img src=$img_src";
+    echo "</img>";
+  }
+  else
+  {
+    echo "Query failed.\n";
+  }
+}
 ?>
 </body>
 </html>
